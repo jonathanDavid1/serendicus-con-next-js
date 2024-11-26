8fe43fee24abb510af45c28e917b97185fcae703
Jonathan David <jhernadezcorrea@gmail.com>
2024-11-26 11:04:26 -0500

    working in details

--- src/components/PropertyDetail.jsx
+++ src/components/PropertyDetail.jsx
@@ -1,3 +1,4 @@
+import WhatsAppIcon from "./WhatsAppIcon"
 const PropertyDetail = ({property}) => {
   return (
     <div className="container mx-auto px-4 max-w-lg">
@@ -7,7 +8,14 @@ const PropertyDetail = ({property}) => {
       <img src={property.image} alt={property.title} className="w-full max-w-lg object-contain" />
       {/* ... */}
     </div>
-    <p className="text-gray-700">{property.description}</p>
+    <p>{property.description}</p>
+    <div>
+    <video width="320" height="240" controls>
+      <source src={property.video} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+    </div>
+    <WhatsAppIcon/>
   </div>
   )
 }
