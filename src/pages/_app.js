e58363816991a35a0373b3c47120a7a6f9f1c3f3
Jonathan David <jhernadezcorrea@gmail.com>
2025-01-07 13:58:11 -0500

    adding code from google ads

---
+++ src/pages/_app.js
@@ -0,0 +1,23 @@
+// En pages/_app.js
+import { Script } from 'next/script';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload" // Carga el script después de que la página esté interactiva
+        src={`https://www.googletagmanager.com/gtag/js?id=G-TG5263QRW7`}
+        onLoad={() => {
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TG5263QRW7');
+        }}
+       
+      />
+        <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,17 +8,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Mystery+Quest&family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap"
           rel="stylesheet"
         />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-TG5263QRW7">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-TG5263QRW7');
-</script>
+  
       </Head>
+
       <body className='bg-black text-white'>
         <Main />
         <NextScript />
